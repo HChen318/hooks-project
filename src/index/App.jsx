60438c23ef7b869a4a3d9abafc27793d02c2f20f
ch250001757 <250001757@qq.com>
2020-06-06 20:45:54 +0800

    feat: 火車票done

--- src/index/App.jsx
+++ src/index/App.jsx
@@ -1,10 +1,132 @@
-import { connect } from 'react-redux'
+import React, { useCallback, useMemo, useEffect } from 'react';
+import { connect } from 'react-redux';
+import Header from '../common/Header.jsx';
+import CitySelector from '../common/CitySelector.jsx';
+import DateSelector from '../common/DateSelector.jsx';
+import Journey from './Journey.jsx';
+import DepartDate from './DepartDate.jsx'
+import HighSpeed from './HighSpeed.jsx'
+import Submit from './Submit.jsx'
 
+import { dateFormat } from '../utils';
 
 import './App.css';
 
+import { bindActionCreators } from 'redux'
+import { 
+  exchangeFromTo, 
+  showCitySelector, 
+  hideCitySelector,
+  fetchCityData,
+  setSelectedCity,
+  showDateSelector,
+  hideDateSelector,
+  setDepartDate,
+  toggleHighSpeed
+ } from './actions'
+
 function App(props) {
-  
+  const {
+    dispatch,
+    from,
+    to,
+    isCitySelectorVisible,
+    cityData,
+    isLoadingCityData,
+    departDate,
+    isDateSelectorVisible,
+    highSpeed
+  } = props
+
+  const onBack = useCallback(() => {
+    window.history.back()
+  }, [])
+
+  // const doExChange = useCallback(() => {
+  //   dispatch(exchangeFromTo())
+  // }, [])
+
+  // const doShowSelector = useCallback((flag) => {
+  //   dispatch(showCitySelector(flag))
+  // }, [])
+
+  useEffect(() => {
+    console.log(111)
+  },[])
+
+  // action和dispatch绑定在一起
+  const cbs = useMemo(() => {
+    return bindActionCreators({
+      exchangeFromTo,
+      showCitySelector
+    }, dispatch)
+  }, [])
+
+  const citySelectorCbs = useMemo(() => {
+    return bindActionCreators({
+      onBack: hideCitySelector,
+      fetchCityData,
+      select:setSelectedCity
+    }, dispatch)
+  }, [])
+
+  const dateCbs = useMemo(() =>{
+    return bindActionCreators({
+      dateClick: showDateSelector,
+    }, dispatch)
+  },[])
+
+  const dateSelectorCbs = useMemo(() =>{
+    return bindActionCreators({
+      onBack: hideDateSelector,
+    }, dispatch)
+  },[])
+
+  const onSelect = useCallback((day) => {
+    if(day < dateFormat()){
+      return
+    }
+    dispatch(setDepartDate(day))
+    dispatch(hideDateSelector())
+  },[])
+
+  const highSpeedCbs = useMemo(() =>{
+    return bindActionCreators({
+      toggleHighSpeed: toggleHighSpeed,
+    }, dispatch)
+  },[])
+
+
+  return (
+    <div>
+      <div className="header-wrapper">
+        <Header title="火车票" onBack={onBack} />
+      </div>
+      <form className="form" action="./query.html">
+        <Journey from={from} to={to}
+          // exchangeFromTo={doExChange}
+          // showCitySelecotr={doShowSelector}
+          {...cbs}
+        />
+        <DepartDate departDate={departDate} {...dateCbs} />
+        <HighSpeed highSpeed={highSpeed}  {...highSpeedCbs} />
+        <Submit />
+      </form>
+
+      <CitySelector
+        show={isCitySelectorVisible}
+        cityData={cityData}
+        isLoading={isLoadingCityData}
+        {...citySelectorCbs}
+      />
+      <DateSelector
+        title="日期选择"
+        {...dateSelectorCbs}
+        onSelect={onSelect}
+        show={isDateSelectorVisible}
+      />
+</div>
+  );
 }
 
 export default connect(
@@ -15,4 +137,3 @@ export default connect(
     return { dispatch }
   }
 )(App)
-
--- src/index/store.js
+++ src/index/store.js
@@ -2,14 +2,25 @@ import {
   createStore,
   combineReducers,
   applyMiddleware,
+  compose
 } from 'redux';
 import thunk from 'redux-thunk';
 import reducers from './reducers'
 
+// Chrome查看redux
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default createStore(
   combineReducers(reducers), {
-
+    from: '北京',
+    to: '上海',
+    isCitySelectorVisible: false, // 城市选择浮层
+    currentSelectingLeftCity: false,
+    cityData: null,
+    isLoadingCityData: false, // 城市加载数据状态
+    departDate: Date.now(),
+    isDateSelectorVisible: false,
+    highSpeed: false,
   },
-  applyMiddleware(thunk),
-) 
+  composeEnhancers(applyMiddleware(thunk)),
+)
